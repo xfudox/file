383fe36c581465f1e4ff1b03f9b403576e988988
SILVIOLENOVO\silvi <silvio.gratani@gmail.com>
2020-03-29 15:46:38 +0200

    refactored tests

---
+++ tests/UnitTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use Orchestra\Testbench\TestCase as TestbenchTestCase;
+use xfudox\File\Models\File;
+
+class UnitTest extends TestbenchTestCase
+{
+
+    const DEFAULT_NAME      = 'default_file.png';
+    const DEFAULT_EXTENSION = 'png';
+    const DEFAULT_MIME      = 'image/png';
+    const DEFAULT_DISK      = 'local';
+    const DEFAULT_PATH      = 'path';
+    const DEFAULT_SIZE      = '1024';
+    
+    public function testCanCreate()
+    {
+        $file = $this->createDefaultFile();
+
+        $this->assertIsObject($file);
+        $this->assertInstanceOf(File::class, $file);
+
+        return $file;
+    }
+
+    /** @depends testCanCreate */
+    public function testCheckAttributes(File $file)
+    {
+        $this->assertEquals(static::DEFAULT_NAME, $file->name);
+        $this->assertEquals(static::DEFAULT_EXTENSION, $file->extension);
+        $this->assertEquals(static::DEFAULT_MIME, $file->mime);
+        $this->assertEquals(static::DEFAULT_DISK, $file->disk);
+        $this->assertEquals(static::DEFAULT_PATH, $file->path);
+        $this->assertEquals(static::DEFAULT_SIZE, $file->size);
+
+        return $file;
+    }
+
+    /** @depends testCheckAttributes */
+    public function testCheckAppendedAttributes(File $file)
+    {
+        $fullname = static::DEFAULT_PATH . '/' . static::DEFAULT_NAME;
+        $this->assertEquals($fullname, $file->fullname);
+
+        return $file;
+    }
+
+    private function createDefaultFile() : File
+    {
+        return new File([
+            'name'      => static::DEFAULT_NAME,
+            'extension' => static::DEFAULT_EXTENSION,
+            'mime'      => static::DEFAULT_MIME,
+            'disk'      => static::DEFAULT_DISK,
+            'path'      => static::DEFAULT_PATH,
+            'size'      => static::DEFAULT_SIZE,
+        ]);
+    }
+}
