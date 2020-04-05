f86e33b898a4904d222a1799477f672a220b3373
SILVIOLENOVO\silvi <silvio.gratani@gmail.com>
2020-04-05 13:54:21 +0200

    refactor

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,12 +9,13 @@ use xfudox\File\FileServiceProvider;
 class TestCase extends TestbenchTestCase
 {
 
-    const DEFAULT_NAME      = 'default_file.png';
-    const DEFAULT_EXTENSION = 'png';
-    const DEFAULT_MIME      = 'image/png';
-    const DEFAULT_DISK      = 'local';
-    const DEFAULT_PATH      = 'path';
-	const DEFAULT_SIZE      = 1024;
+	const DEFAULT_NAME			= 'default_file.png';
+	const DEFAULT_EXTENSION		= 'png';
+	const DEFAULT_MIME			= 'image/png';
+	const DEFAULT_DISK			= 'default_disk';
+	const DEFAULT_PATH			= 'path';
+	const DEFAULT_SIZE			= 1024;
+	const SECOND_DISK			= 'second_disk';
 	
 	protected function getPackageProviders($app)
 	{
@@ -26,19 +27,41 @@ class TestCase extends TestbenchTestCase
 		// Setup default database to use sqlite :memory:
 		$app['config']->set('database.default', 'testbench');
 		$app['config']->set('database.connections.testbench', [
-			'driver'   	=> 'sqlite',
+			'driver'	=> 'sqlite',
 			'database' 	=> ':memory:',
-			'prefix'   	=> '',
+			'prefix'	=> '',
 		]);
 	}
 
 	public function setUp(): void
 	{
 		parent::setUp();
-
-		$this->artisan('migrate', 
-					['--database' => 'testbench'])->run();
+		$this->runDefaultMigrations();
+		$this->setUpDisks();
 		
+	}
+
+	private function runDefaultMigrations()
+	{
+		$this->artisan('migrate', 
+			['--database' => 'testbench'])->run();
+	}
+
+	private function setUpDisks()
+	{
 		Storage::fake(static::DEFAULT_DISK);
+		$this->clearDisk(static::DEFAULT_DISK);
+		Storage::fake(static::SECOND_DISK);
+		$this->clearDisk(static::SECOND_DISK);
+	}
+
+	private function clearDisk(?string $disk = null)
+	{
+		$disk = $disk ?? static::DEFAULT_DISK;
+		
+		$files = Storage::disk($disk)->allFiles('/');
+		foreach($files as $file){
+			Storage::disk($disk)->delete($file);
+		}
 	}
 }
