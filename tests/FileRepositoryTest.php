4536357580b76c2cc367651f44a1c663d6b1c439
SILVIOLENOVO\silvi <silvio.gratani@gmail.com>
2020-04-13 11:36:37 +0200

    refactoretd tests

--- tests/FileRepositoryTest.php
+++ tests/FileRepositoryTest.php
@@ -2,29 +2,16 @@
 
 namespace xfudox\File\Tests;
 
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Storage;
 use xfudox\File\Models\File;
-use xfudox\File\Repositories\FileRepository;
 use xfudox\File\Tests\TestCase;
 
 class FileRepositoryTest extends TestCase
 {
-
-    private $repository;
-
-    public function setUp() : void
-    {
-        parent::setUp();
-        $this->repository = App::make(FileRepository::class);
-    }
     
-    public function testCreateFromUploadedFile()
+    public function testCreateFromUploadedFileToDefaultLocation()
     {
-        $uploaded_file = UploadedFile::fake()->image('uploaded_image.png');
-
-        $file = $this->repository->createFromUploadedFile($uploaded_file);
+        $file = $this->test_file;
 
         $this->assertIsObject($file);
         $this->assertInstanceOf(File::class, $file);
@@ -34,27 +21,41 @@ class FileRepositoryTest extends TestCase
         $this->assertEquals(Storage::disk($file->disk)->size($file->fullname), $file->size);
         $this->assertEquals(Storage::disk($file->disk)->mimeType($file->fullname), $file->mime);
         $this->assertEquals(Storage::disk($file->disk)->size($file->fullname), $file->size);
-
-        return $file;
     }
 
-    /** @depends testCreateFromUploadedFile */
-    public function testGetFileUrl(File $file)
+    /** @depends testCreateFromUploadedFileToDefaultLocation */
+    public function testFileLocationIsDefaultDiskRoot()
     {
+        $file = $this->test_file;
+        
+        $this->assertEquals(static::DEFAULT_DISK, $file->disk);
+        $this->assertEquals('', $file->path);
+    }
+
+    /** @depends testCreateFromUploadedFileToDefaultLocation */
+    public function testGetFileUrl()
+    {
+        $file = $this->test_file;
+        
         $expected_url   = Storage::disk($file->disk)->url($file->fullname);
-        $actual_url     = $this->repository->getFileUrl($file);
+        $actual_url     = $this->test_repository->getFileUrl($file);
 
         $this->assertEquals($expected_url, $actual_url);
     }
 
-    /** @depends testCreateFromUploadedFile */
-    public function testExists(File $file)
+    /** @depends testCreateFromUploadedFileToDefaultLocation */
+    public function testFileExistsOnStorage()
     {
-        $actual     = $this->repository->exists($file);
+        $file = $this->test_file;
+        
+        $actual     = $this->test_repository->exists($file);
         $expected   = Storage::disk($file->disk)->exists($file->fullname);
         $this->assertTrue($expected);
         $this->assertEquals($expected, $actual);
+    }
 
+    public function testFileDontExistsOnStorage()
+    {
         $missing_file = File::create([
             'name'      => 'missing_image.png',
             'extension' => 'png',
@@ -63,46 +64,44 @@ class FileRepositoryTest extends TestCase
             'path'      => '/',
             'size'      => 512,
         ]);
-        $actual     = $this->repository->exists($missing_file);
+        $actual     = $this->test_repository->exists($missing_file);
         $expected   = Storage::disk($missing_file->disk)->exists($missing_file->fullname);
         $this->assertFalse($expected);
         $this->assertEquals($expected, $actual);
-
-        return $file;
     }
 
-    /** @depends testCreateFromUploadedFile */
-    public function testMoveFileOnSameDisk(File $file)
+    /** @depends testCreateFromUploadedFileToDefaultLocation */
+    public function testMoveFileOnSameDisk()
     {
+        $file = $this->test_file;
+        
         $source      = $file->fullname;
         $path        = 'dir';
         $destination = "{$path}/{$file->name}";
 
-        $this->repository->moveFile($file, $destination);
+        $this->test_repository->moveFile($file, $destination);
 
         $this->assertEquals($path, $file->path);
         $this->assertEquals($destination, $file->fullname);
         Storage::disk($file->disk)->assertExists($destination);
         Storage::disk($file->disk)->assertMissing($source);
-
-        return $file;
     }
 
     /** @depends testMoveFileOnSameDisk */
-    public function testMoveFileOnDifferentDisks(File $file)
+    public function testMoveFileOnDifferentDisks()
     {
+        $file = $this->test_file;
+        
         $disk        = static::SECOND_DISK;
         $path        = 'directory';
         $destination = "{$path}/{$file->name}";
 
-        $this->repository->moveFile($file, "{$disk}::" . $destination);
+        $this->test_repository->moveFile($file, "{$disk}::" . $destination);
 
         $this->assertEquals($disk, $file->disk);
         $this->assertEquals($path, $file->path);
         $this->assertEquals($destination, $file->fullname);
         Storage::disk($file->disk)->assertExists($destination);
-
-        return $file;
     }
 
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,20 +2,22 @@
 
 namespace xfudox\File\Tests;
 
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Storage;
 use Orchestra\Testbench\TestCase as TestbenchTestCase;
 use xfudox\File\FileServiceProvider;
+use xfudox\File\Models\File;
+use xfudox\File\Repositories\FileRepository;
 
 class TestCase extends TestbenchTestCase
 {
 
-	const DEFAULT_NAME			= 'default_file.png';
-	const DEFAULT_EXTENSION		= 'png';
-	const DEFAULT_MIME			= 'image/png';
-	const DEFAULT_DISK			= 'default_disk';
-	const DEFAULT_PATH			= 'path';
-	const DEFAULT_SIZE			= 1024;
-	const SECOND_DISK			= 'second_disk';
+	const DEFAULT_DISK	= 'default_disk';
+	const SECOND_DISK	= 'second_disk';
+
+	protected $test_file;
+    protected $test_repository;
 	
 	protected function getPackageProviders($app)
 	{
@@ -23,6 +25,27 @@ class TestCase extends TestbenchTestCase
 	}
 
 	protected function getEnvironmentSetUp($app)
+	{
+		$this->getDatabaseConfig($app);
+		$this->getStorageConfig($app);
+	}
+
+	public function setUp(): void
+	{
+		parent::setUp();
+		$this->runDefaultMigrations();
+		$this->setUpStorage();
+		$this->createTestRepository();
+		$this->createTestFile();
+	}
+
+	private function runDefaultMigrations()
+	{
+		$this->artisan('migrate', 
+			['--database' => 'testbench'])->run();
+	}
+
+	private function getDatabaseConfig($app)
 	{
 		// Setup default database to use sqlite :memory:
 		$app['config']->set('database.default', 'testbench');
@@ -33,35 +56,34 @@ class TestCase extends TestbenchTestCase
 		]);
 	}
 
-	public function setUp(): void
+	private function getStorageConfig($app)
 	{
-		parent::setUp();
-		$this->runDefaultMigrations();
-		$this->setUpDisks();
-		
+		$app['config']->set('filesystems.default', static::DEFAULT_DISK);
 	}
 
-	private function runDefaultMigrations()
-	{
-		$this->artisan('migrate', 
-			['--database' => 'testbench'])->run();
-	}
-
-	private function setUpDisks()
+	private function setUpStorage()
 	{
 		Storage::fake(static::DEFAULT_DISK);
-		$this->clearDisk(static::DEFAULT_DISK);
 		Storage::fake(static::SECOND_DISK);
-		$this->clearDisk(static::SECOND_DISK);
 	}
 
-	private function clearDisk(?string $disk = null)
+	private function createTestRepository()
 	{
-		$disk = $disk ?? static::DEFAULT_DISK;
+        $this->test_repository = App::make(FileRepository::class);
+	}
+
+	private function createTestFile()
+	{
+		$uploaded_file = UploadedFile::fake()->image('uploaded_image.png');
+		$uploaded_file->storeAs('/', $uploaded_file->getClientOriginalName(), ['disk' => static::DEFAULT_DISK]);
 		
-		$files = Storage::disk($disk)->allFiles('/');
-		foreach($files as $file){
-			Storage::disk($disk)->delete($file);
-		}
+		$this->test_file = File::create([
+            'name'      => $uploaded_file->getClientOriginalName(),
+            'extension' => $uploaded_file->getClientOriginalExtension(),
+            'mime'      => $uploaded_file->getClientMimeType(),
+            'size'      => $uploaded_file->getSize(),
+            'disk'      => static::DEFAULT_DISK,
+            'path'      => '/',
+        ]);
 	}
 }
